b0457a05c00ab5d99d666a96bcb74c63b806b536
tye-beach <tye@technink.com>
2020-01-23 16:05:36 -0500

    First Commit

---
+++ Percy/Percy/Modules/Commands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace TutorialBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+    }
+}
